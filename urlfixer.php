11fbe38042c50cdce1bc972a21242564faa57798
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-07-03 14:41:32 +0200

    af

--- urlfixer.php
+++ urlfixer.php
@@ -1,12 +1,12 @@
 <?php
 
 $input = $argv[1];
-$patternIncorrectdomain = '/(?<protocol>http[s]{0,1}):\/{0,1}(?<slashes>\\\\{0,})\/{0,}(?<domain>[^\\\\]{0,})(?<moreslashes>\\\\{0,})/m';
+$patternIncorrectdomain = '/(?<protocol>http[s]{0,1}):\/{0,1}(?<slashes>\\\\{0,})\/{0,}(?<domain>[^\\\\^\/]{0,})(?<moreslashes>\\\\{0,})\/{0,}(?<path>[^\\\\^\/]{0,})/m';
 
 
 //checks if correct protocol is given
 if (preg_match($patternIncorrectdomain, $input, $matches)) {
-    $replacementUrl = "https://" . $matches["domain"] . "/";
+    $replacementUrl = "https://" . $matches["domain"] . "/" . $matches["path"];
     echo preg_replace($patternIncorrectdomain, $replacementUrl, $input);
 } else {
     echo("nothing to replace");
